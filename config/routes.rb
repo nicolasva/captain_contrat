228bd8074e5a0c5c613f28806fdcb4195a7d797c
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-07-10 14:49:12 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :fights
-  resources :shields
   resources :heros
+  resources :ranks do
+    resources :fights
+  end
+  #resources :shield
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
