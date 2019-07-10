be62fe1be65963934b1b6c9f149400e1ad5b68be
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-07-11 00:18:51 +0200

    update fight_event

--- app/helpers/fights_helper.rb
+++ app/helpers/fights_helper.rb
@@ -1,2 +1,12 @@
 module FightsHelper
+  def details(event)
+    result = "#{event.attacker_name} attaque pour #{event.attack_damage} dégâts."
+    result += " C'est un coup critique !" if event.attack_type == 'critical'
+
+    result += " #{event.defender_name} subit l'attaque" if event.defend_type == 'receive'
+    result += " #{event.defender_name} évite l'attaque" if event.defend_type == 'dodge'
+
+    result += " et a #{event.defender_health} points de vie !"
+    return result
+  end
 end
--- app/models/fight_event.rb
+++ app/models/fight_event.rb
@@ -4,13 +4,4 @@ class FightEvent < ApplicationRecord
   enum attack_type: ["normal", "critical"]
   enum defend_type: ["receive", "dodge"]
 
-  def to_str
-    event = "#{attacker_name} attaque pour #{attack_damage} dégâts."
-    event += " C'est un coup critique !" if attack_type == 'critical'
-
-    event += " #{defender_name} subit l'attaque" if defend_type == 'receive'
-    event += " #{defender_name} évite l'attaque" if defend_type == 'dodge'
-
-    event += " et a #{defender_health} points de vie !"
-  end
 end
